8d496d3bcdc7b7ece4cd713c912d7115728ebf28
AlexeyGulis <kiri.dh@yandex.ru>
2021-07-08 20:25:16 +0300

    Add hello world

---
+++ src/by/academy/lesson2/ScanDemo.java
@@ -0,0 +1,9 @@
+package by.academy.lesson2;
+
+import java.util.Scanner;
+
+public class ScanDemo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+}
